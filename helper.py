dafdbcef9fb1671800c96a365af8825b1d6df574
mSafa <safaktrcgl@gmail.com>
2022-11-16 19:39:45 +0300

    Set x min to 0 for better graph visual

--- helper.py
+++ helper.py
@@ -13,6 +13,7 @@ def plot(scores, mean_scores, best_scores):
     plt.plot(scores)
     plt.plot(mean_scores)
     plt.plot(best_scores)
+    plt.xlim(xmin=0)
     plt.ylim(ymin=0)
     plt.text(len(scores)-1, scores[-1], "Score: " + str(scores[-1]))
     plt.text(len(mean_scores)-1, mean_scores[-1], "Mean: " + str(mean_scores[-1]))
